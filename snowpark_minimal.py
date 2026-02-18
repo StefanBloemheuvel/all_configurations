b10e1ce21e60a60459ea620cbfdaa30949dcdc39
Stefan Bloemheuvel <40235998+StefanBloemheuvel@users.noreply.github.com>
2026-02-18 14:38:42 +0100

    Update snowpark_minimal.py

--- snowpark_minimal.py
+++ snowpark_minimal.py
@@ -13,3 +13,19 @@ spark = Session.builder.configs(config).create()
 
 
 spark.table('personal_workspaces.sbloemheuvel.booker_preds_v1').show()
+
+
+
+#### OR #####
+from snowflake.snowpark import Session
+import snowflake.snowpark.functions as F
+import bkng
+from bkng.bdx.context import Context
+from bkng.bdx.snowflake import connect as bdx_snowflake_connect
+
+ctx = Context.build()                          
+sf_conn = bdx_snowflake_connect(ctx)           
+
+snowpark_session = Session.builder.configs({"connection": sf_conn}).getOrCreate()
+
+df = snowpark_session.table("fpa_sagemaker_bdx_dev.expt.utilization_preds_v2").toPandas()
